e875f3ecf52dcaaf7906be581f36c845e394d778
Conor Sullivan <conorwsullivan@gmail.com>
2018-03-30 07:32:54 +0000

    Install termite

--- bin/install_ubuntu.sh
+++ bin/install_ubuntu.sh
@@ -1,3 +1,4 @@
+# Neovim
 #sudo apt-get install software-properties-common
 #sudo add-apt-repository ppa:neovim-ppa/unstable
 sudo apt-get update
@@ -15,3 +16,18 @@ go get -d github.com/pocke/lemonade
 cd $GOPATH/src/github.com/pocke/lemonade/
 make install
 ln -s /usr/bin/nvim /usr/local/bin/vim
+
+# Termite
+cd ~/work
+git clone --recursive https://github.com/thestinger/termite.git
+git clone https://github.com/thestinger/vte-ng.git
+
+echo export LIBRARY_PATH="/usr/include/gtk-3.0:$LIBRARY_PATH"
+cd vte-ng && ./autogen.sh && make && sudo make install
+cd ../termite && make && sudo make install
+sudo ldconfig
+sudo mkdir -p /lib/terminfo/x; sudo ln -s \
+/usr/local/share/terminfo/x/xterm-termite \
+/lib/terminfo/x/xterm-termite
+
+sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator /usr/local/bin/termite 60
