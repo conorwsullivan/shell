48e617205a44d20c3e7f39bc349a9c2b21a73ab5
Conor Sullivan <conorwsullivan@gmail.com>
2018-03-30 00:29:20 -0700

    Updating neovim installation

--- bin/install_ubuntu.sh
+++ bin/install_ubuntu.sh
@@ -1,13 +1,13 @@
 #sudo apt-get install software-properties-common
 #sudo add-apt-repository ppa:neovim-ppa/unstable
-#sudo apt-get update
-#sudo apt-get install neovim golang
-#sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
-#sudo update-alternatives --set vi /usr/bin/nvim
-#sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
-#sudo update-alternatives --set vim /usr/bin/nvim
-#sudo update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 60
-#sudo update-alternatives --set editor /usr/bin/nvim
+sudo apt-get update
+sudo apt-get install neovim golang
+sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
+sudo update-alternatives --set vi /usr/bin/nvim
+sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
+sudo update-alternatives --set vim /usr/bin/nvim
+sudo update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 60
+sudo update-alternatives --set editor /usr/bin/nvim
 sudo apt install rpl
 GOPATH=~/gocade
 export GOPATH=$HOME/gocade
